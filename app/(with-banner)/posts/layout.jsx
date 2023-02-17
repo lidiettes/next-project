3bc0c4fe7b326457639b6c4acd3fae0af2e6eb9c
lidiettes <hello@lidiettes.com>
2023-02-17 10:33:40 +0100

    marquee added

--- app/(with-banner)/posts/layout.jsx
+++ app/(with-banner)/posts/layout.jsx
@@ -1,9 +1,10 @@
 import { Counter } from './Counter.jsx'
 
-export default function PostsLayout({ children }) {
+export default function Layout({ children }) {
     return (
         <div>
             <Counter />
+            <marquee style={{background: '#fff', color: 'red', fontSize: '3rem'}}>Eres una crack po po po</marquee>
             {children}
         </div>
     )
